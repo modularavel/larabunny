be1936fb5238b17453414283d14d08ea4fb1f054
Casimiro Rocha <casimiroaf@gmail.com>
2025-08-26 03:43:01 -0300

    style: Update .editorconfig indent size chore: Remove unused .gitignore files from workbench/storage feat: Add home.blade.php view file refactor: Improve formatting in config/larabunny.php fix: Remove unused Larabunny test route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Welcome to my website</title>
+    </head>
+    <body>
+        <h1>Welcome to my website</h1>
+        <p>This is a sample website created using Laravel.</p>
+    </body>
+</html>
--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modularavel\Larabunny\Facades\Larabunny;
 
 Route::get('/', function () {
-    $test = Larabunny::deleteCollection(
-       config('larabunny.library_id'),
-       '4b740d15-e32f-4ba7-b038-cef24b2ae10b',
-   );
-
-    dd($test);
-
     return view('welcome');
 });
