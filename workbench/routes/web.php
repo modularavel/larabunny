c579a871e7b6c13e794d88e1a96483c7248369dc
casimirorocha <6707152+casimirorocha@users.noreply.github.com>
2025-08-26 06:42:36 +0000

    Fix styling

--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -5,9 +5,9 @@ use Modularavel\Larabunny\Facades\Larabunny;
 
 Route::get('/', function () {
     $test = Larabunny::deleteCollection(
-       config('larabunny.library_id'),
-       '4b740d15-e32f-4ba7-b038-cef24b2ae10b',
-   );
+        config('larabunny.library_id'),
+        '4b740d15-e32f-4ba7-b038-cef24b2ae10b',
+    );
 
     dd($test);
 
